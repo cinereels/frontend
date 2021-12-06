bcf6124ca01c84d927a2feefd862abcd494fbcc6
Lakshay Grover <lakshaygrover7@gmail.com>
2021-12-06 22:39:25 +0530

    movie

--- src/store/action-types.js
+++ src/store/action-types.js
@@ -1,4 +1,3 @@
 export const AUTH_SUCCESS = "AUTH_SUCCESS";
 export const AUTH_LOGOUT = "AUTH_LOGOUT";
-export const GET_MOVIE = "GET_MOVIE";
-export const POST_MOVIE = "POST_MOVIE";
+export const SET_MOVIES = "SET_MOVIES";
